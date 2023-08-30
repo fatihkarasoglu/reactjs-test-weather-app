2edce978bab6a4341c11e3fd6d7bd1f7f40456b4
fatihkarasoglu <f.karasoglu.01@gmail.com>
2023-08-30 21:09:11 +0300

    Some changes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
-import { useState, useEffect } from "react";
 import axios from "axios";
+import { useState, useEffect } from "react";
 
-import Location from "./components/Location";
+import Location from "./Location";
 
 function App() {
 
-  const data = '48afb245d0fe10f13555b750e33fb5ba';
+  const apiKey = '48afb245d0fe10f13555b750e33fb5ba';
 
-  const [search, setSearch] = useState();
+  const [search, setSearch] = useState("");
   const [location, setLocation] = useState();
 
   useEffect(() => {
-    async function getData() {
-      try{
-        const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&lang=tr&appid=${data}&units=metric`);
-        setLocation(res.data);
-      } catch(err) {
-        console.log(err);
+    async function getUser() {
+      try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${apiKey}&units=metric&lang=tr`);
+        console.log(response)
+        setLocation(response.data)
+      } catch (error) {
+        console.error(error);
       }
     }
-    getData();
+    getUser();
   }, [search]);
 
   return (
-    <div className="">
-      <input onChange={(e) => setSearch(e.target.value)} type="text" />
-      {location && <Location location={location} />}
+    <div className="min-w-full min-h-screen flex justify-center">
+      <div className="">
+        <input onChange={(e) => setSearch(e.target.value)} placeholder="Konumunuzu girin..." type="text" className="px-3 py-3 text-slate-800 border-0 shadow rounded placeholder-blue-400 w-[250px]"/>
+        {location && <Location location={location} />}
+      </div>
     </div>
   );
 }
