8e5ddc55530b317eb1f532de7326984279c6c5ac
fatihkarasoglu <f.karasoglu.01@gmail.com>
2023-09-04 03:23:00 +0300

    End of project.

--- src/App.js
+++ src/App.js
@@ -23,13 +23,19 @@ function App() {
     getUser();
   }, [search]);
 
+  const resetHandle = () => {
+    setLocation(null);
+    setSearch('');
+  }
+
   return (
     <div className="min-w-full min-h-screen flex flex-col items-start justify-end bg-[url('./images/lake.jpg')] bg-cover">
       <div>
-        <input onChange={(e) => setSearch(e.target.value)} placeholder="Konum girin..." type="text" className="mb-60 ml-2 px-3 py-3 text-green-700 bg-green-300 border-0 shadow rounded placeholder-green-700 w-[250px] outline-none"/>
+        <input onChange={(e) => setSearch(e.target.value)} value={search} placeholder="Konum girin..." type="text" className="mb-60 ml-2 px-3 py-3 text-green-700 bg-green-300 border-0 shadow rounded placeholder-green-700 w-[250px] outline-none"/>
+        <button type="reset" onClick={resetHandle} className="px-3 py-3 text-red-800 bg-red-300 ml-2 hover:bg-red-400">Sıfırla</button>
       </div>
       {location && <Location location={location} />}
-      <div class='absolute bottom-0 w-full h-2/6 bg-black opacity-50'></div>
+      <div className='absolute bottom-0 w-full h-2/6 bg-black opacity-50'></div>
     </div>
   );
 }
